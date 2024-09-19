dadb90fab666128d0fd048ca1f0804825f821d70
ashikurriyal <riyal.ashikur@gmail.com>
2024-09-19 11:29:17 +0600

    Initialize the project set up with React router dom, tailwind css and daisy UI. Updated the router setup and Layout

---
+++ src/Components/Layout/MainLayout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/Navbar/Navbar";
+import Footer from "../Pages/Footer/Footer";
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className="min-h-[calc(100vh-387px)]">
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default MainLayout;
