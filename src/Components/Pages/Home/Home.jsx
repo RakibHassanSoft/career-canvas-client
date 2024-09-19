8dcb7ca5dafd0f6672e0609e47eb1d0265f3371e
ashikurriyal <riyal.ashikur@gmail.com>
2024-09-19 12:50:41 +0600

    Updated the tailwind and daisy

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@
 const Home = () => {
     return (
         <div>
-            <h1 className="">This is Home</h1>
+            <h1 className="text-4xl text-yellow-500">This is Home</h1>
         </div>
     );
 };
