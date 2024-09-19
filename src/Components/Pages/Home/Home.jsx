7324a94a039b931060e5f8f7ae8776e930042bda
ashikurriyal <riyal.ashikur@gmail.com>
2024-09-19 20:03:41 +0600

    Updated the why choose us section with its responsive view in Home Page

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,12 +1,16 @@
-import Banner from "./Banner/Banner";
 
+import Banner from "./Banner/Banner";
+import ChooseOurPlatform from "./ChooseOurPlatform/ChooseOurPlatform";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      <div className="container mx-auto mt-20">
+        <ChooseOurPlatform></ChooseOurPlatform>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
