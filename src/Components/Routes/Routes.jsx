08afdc6f8f8e1b31469df3ed7f7c14bc7271b2be
emonkhan500 <khdilo47@gmail.com>
2024-09-19 16:29:28 +0600

    signIn & signUp page complete

--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -1,6 +1,8 @@
 import {createBrowserRouter} from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
+import SingIn from "../Authentication/SingIn";
+import SignUp from "../Authentication/SignUp";
 
   
   const router = createBrowserRouter([
@@ -11,7 +13,15 @@ import Home from "../Pages/Home/Home";
         {
             path: '/',
             element: <Home></Home>
-        }
+        },
+        {
+          path:'/signin',
+          element:<SingIn></SingIn>,
+      },
+      {
+          path:'/signup',
+          element:<SignUp></SignUp>,
+      },
       ]
     },
   ]);
