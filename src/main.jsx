8074d7614aaa28f4579ddd4838b0a27670771e73
ashikurriyal <riyal.ashikur@gmail.com>
2024-09-26 01:26:03 +0600

    updated the main jsx with authprovider rapping

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import router from './Components/Routes/Routes'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./Components/Routes/Routes";
+import "./index.css";
+import AuthProvider from "./Components/Providers/AuthProvider";
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </AuthProvider>
+);
