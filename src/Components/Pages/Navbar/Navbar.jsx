425b59c431decc392e24aac341c4cfb38e322f88
RakibHassanSoft <rakibreings32@gmail.com>
2024-10-10 18:55:32 +0600

    admin and user file is added with 5 cards section

--- src/Components/Pages/Navbar/Navbar.jsx
+++ src/Components/Pages/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProvider";
 import { toast } from "react-toastify";
@@ -6,7 +6,11 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
+  const handleDropdownToggle = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  }
   const handleLogOut = () => {
     logOut()
       .then(() => {
@@ -61,17 +65,20 @@ const Navbar = () => {
                 tabIndex={0}
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
               >
-                <NavLink
-                  to="/resume-templates/personal-info-form"
-                  className={({ isActive }) =>
-                    `ml-4 ${isActive
-                      ? "font-extrabold bg-gradient-to-r from-green-500 to-slate-500 bg-clip-text text-transparent text-lg border-b-2 border-green-500"
-                      : "text-lg font-semibold"
-                    }`
-                  }
-                >
-                  Personal Info
-                </NavLink>
+                {
+                  user && <NavLink
+                    to="/resume-templates/personal-info-form"
+                    className={({ isActive }) =>
+                      `ml-4 ${isActive
+                        ? "font-extrabold bg-gradient-to-r from-green-500 to-slate-500 bg-clip-text text-transparent text-lg border-b-2 border-green-500"
+                        : "text-lg font-semibold"
+                      }`
+                    }
+                  >
+                    Personal Info
+                  </NavLink>
+                }
+
 
                 <NavLink
                   to="/resume-templates/languages-form"
@@ -120,19 +127,19 @@ const Navbar = () => {
               Resume Review
             </NavLink>
             {
-              user &&   <NavLink
-              to="/user-profile"
-              className={({ isActive }) =>
-                `ml-4 ${isActive
-                  ? "font-extrabold bg-gradient-to-r from-green-500 to-slate-500 bg-clip-text text-transparent text-lg border-b-2 border-green-500"
-                  : "text-lg font-semibold"
-                }`
-              }
-            >
-              User Profile
-            </NavLink>
+              user && <NavLink
+                to="/user-profile"
+                className={({ isActive }) =>
+                  `ml-4 ${isActive
+                    ? "font-extrabold bg-gradient-to-r from-green-500 to-slate-500 bg-clip-text text-transparent text-lg border-b-2 border-green-500"
+                    : "text-lg font-semibold"
+                  }`
+                }
+              >
+                User Profile
+              </NavLink>
             }
-          
+
           </ul>
         </div>
         <div className="hidden lg:flex justify-center items-center">
@@ -162,25 +169,39 @@ const Navbar = () => {
         >
           Home
         </NavLink>
-        <div className="dropdown dropdown-hover">
-          <label tabIndex={0} className="ml-4 cursor-pointer text-lg font-semibold">
-            Resume Templates
-          </label>
-          <ul className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-            <NavLink
-              to="/resume-templates/Resume_Templates"
-              className={({ isActive }) =>
-                `ml-4 ${isActive
-                  ? "font-extrabold bg-gradient-to-r from-green-500 to-slate-500 bg-clip-text text-transparent text-lg border-b-2 border-green-500"
-                  : "text-lg font-semibold"
-                }`
-              }
-            >
-              Make resume
-            </NavLink>
 
-          </ul>
-        </div>
+        {
+          user && <div className="dropdown dropdown-hover">
+            <label tabIndex={0} className="ml-4 cursor-pointer text-lg font-semibold">
+              Resume Templates
+            </label>
+            <ul className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+              <NavLink
+                to="/resume-templates/Resume_Templates"
+                className={({ isActive }) =>
+                  `ml-4 ${isActive
+                    ? "font-extrabold bg-gradient-to-r from-green-500 to-slate-500 bg-clip-text text-transparent text-lg border-b-2 border-green-500"
+                    : "text-lg font-semibold"
+                  }`
+                }
+              >
+                Make resume
+              </NavLink>
+              <NavLink
+                to="/drag-and-drop"
+                className={({ isActive }) =>
+                  `ml-4 ${isActive
+                    ? "font-extrabold bg-gradient-to-r from-green-500 to-slate-500 bg-clip-text text-transparent text-lg border-b-2 border-green-500"
+                    : "text-lg font-semibold"
+                  }`
+                }
+              >
+                Drag and drop
+              </NavLink>
+
+            </ul>
+          </div>
+        }
         <NavLink
           to="/job-posting"
           className={({ isActive }) =>
@@ -214,42 +235,106 @@ const Navbar = () => {
         >
           Resume Review
         </NavLink>
-        <NavLink
-          to="/user-profile"
-          className={({ isActive }) =>
-            `ml-4 ${isActive
-              ? "font-extrabold bg-gradient-to-r from-green-500 to-slate-500 bg-clip-text text-transparent text-lg border-b-2 border-green-500"
-              : "text-lg font-semibold"
-            }`
-          }
-        >
-          User Profile
-        </NavLink>
-        <NavLink
-          to="/ai-chat"
-          className={({ isActive }) =>
-            `ml-4 ${isActive
-              ? "font-extrabold bg-gradient-to-r from-green-500 to-slate-500 bg-clip-text text-transparent text-lg border-b-2 border-green-500"
-              : "text-lg font-semibold"
-            }`
-          }
-        >
-          CC Ai
-        </NavLink>
+        {
+          user && <NavLink
+            to="/user-profile"
+            className={({ isActive }) =>
+              `ml-4 ${isActive
+                ? "font-extrabold bg-gradient-to-r from-green-500 to-slate-500 bg-clip-text text-transparent text-lg border-b-2 border-green-500"
+                : "text-lg font-semibold"
+              }`
+            }
+          >
+            User Profile
+          </NavLink>
+        }
+
+        {
+          user && <NavLink
+            to="/ai-chat"
+            className={({ isActive }) =>
+              `ml-4 ${isActive
+                ? "font-extrabold bg-gradient-to-r from-green-500 to-slate-500 bg-clip-text text-transparent text-lg border-b-2 border-green-500"
+                : "text-lg font-semibold"
+              }`
+            }
+          >
+            CC Ai
+          </NavLink>
+        }
+        {
+          user && <NavLink
+            to="/job-section"
+            className={({ isActive }) =>
+              `ml-4 ${isActive
+                ? "font-extrabold bg-gradient-to-r from-green-500 to-slate-500 bg-clip-text text-transparent text-lg border-b-2 border-green-500"
+                : "text-lg font-semibold"
+              }`
+            }
+          >
+            Job posts
+          </NavLink>
+        }
+
       </div>
       <div className="navbar-end">
         {user ? (
-          <>
-            <Link to="/user-profile">
-              <span className="font-bold text-lg">{user.displayName}</span>
-            </Link>
+          <div className="relative inline-block text-left">
+            <div
+              className="flex items-center cursor-pointer"
+              onClick={handleDropdownToggle}
+            >
+              <Link to="/user-profile">
+                <span className="font-bold text-lg">{user.displayName}</span>
+              </Link>
+              <svg
+                className="w-5 h-5 ml-2"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                ></path>
+              </svg>
+            </div>
 
-
-            <button onClick={handleLogOut} className="ml-2 px-5 py-2.5 relative rounded group overflow-hidden font-bold text-lg bg-purple-50 text-green-500 inline-block">
-              <span className="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 border-2 border-green-500 bg-green-500 group-hover:h-full opacity-90"></span>
-              <span className="relative group-hover:text-white">Log Out</span>
-            </button>
-          </>
+            {/* Dropdown Menu */}
+            {isDropdownOpen && (
+              <div className="absolute right-0 mt-2 w-48 bg-white border rounded shadow-lg z-10">
+                <ul className="py-1">
+                  <li>
+                    <Link
+                      to="/dashboard"
+                      className="block px-4 text-lg py-2 text-gray-700 hover:bg-gray-200"
+                    >
+                      Dashboard
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/user-profile"
+                      className="block px-4 text-lg py-2 text-gray-700 hover:bg-gray-200"
+                    >
+                      Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <button
+                      onClick={handleLogOut}
+                      className="block w-full text-lg text-left px-4 py-2 text-gray-700 hover:bg-gray-200"
+                    >
+                      Log Out
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
         ) : (
           <>
             {/* sign in button */}
