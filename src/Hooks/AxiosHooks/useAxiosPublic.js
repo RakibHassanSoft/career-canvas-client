87f9fc636dee1930cf6030aaf558aa00f2aa2442
RakibHassanSoft <rakibreings32@gmail.com>
2024-10-10 18:56:54 +0600

    Server is added

--- src/Hooks/AxiosHooks/useAxiosPublic.js
+++ src/Hooks/AxiosHooks/useAxiosPublic.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosPublic = axios.create({
     // baseURL: '/'
-    // baseURL: 'https://clean-server.onrender.com'
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://clean-server.onrender.com'
+    // baseURL: 'http://localhost:5000'
 })
 
 const useAxiosPublic = () => {
